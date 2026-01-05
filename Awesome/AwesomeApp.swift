387e97a86d48f1adca4f18ea9c70cb8df4ce48bf
Steve Kite <skite1@hotmail.com>
2026-01-05 14:55:54 -0800

    Initial Commit

---
+++ Awesome/AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeApp.swift
+//  Awesome
+//
+//  Created by Steve on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Awesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Awesome
+//
+//  Created by Steve on 1/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
